d5f3c9b028e02c6e92234e8850d66fa12477a706
ManzDev <JoseRomanHernandez@gmail.com>
2023-09-22 18:22:35 +0100

    Update packages and linters

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-const isGitHubPages = true;
-const path = require("path");
-const folderName = path.basename(process.cwd()) + "/";
-const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
+import { defineConfig } from "vite";
+import path from "node:path";
 
-module.exports = {
+const isGitHubPages = true;
+const folderName = `${path.basename(process.cwd())}/`;
+const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const base = mode === "production" && isGitHubPages ? `/${folderName}` : "/";
+
+export default defineConfig({
   root: "src",
   base,
   mode,
+  envDir: "../",
   publicDir: "../public",
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname
+    }
+  },
   build: {
     outDir: "../dist",
     assetsDir: "./"
   }
-};
+});
